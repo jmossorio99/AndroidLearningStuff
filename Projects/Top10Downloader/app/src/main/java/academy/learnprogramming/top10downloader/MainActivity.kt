03eb571fc797dfe0fc22a224689b21d086da88e4
Jose Ossorio <joseossorio99@hotmail.com>
2020-01-12 12:10:30 -0500

    start of Youtube App

--- Projects/Top10Downloader/app/src/main/java/academy/learnprogramming/top10downloader/MainActivity.kt
+++ Projects/Top10Downloader/app/src/main/java/academy/learnprogramming/top10downloader/MainActivity.kt
@@ -1,12 +1,17 @@
 package academy.learnprogramming.top10downloader
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
+import kotlin.properties.Delegates
 
-class FeedEntry{
+class FeedEntry {
 
     var name: String = ""
     var artist: String = ""
@@ -33,33 +38,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: called")
 
-        val downloadData = DownloadData()
+        val downloadData = DownloadData(this, xmlListView)
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
     }
 
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>(){
+        private class DownloadData(context: Context, listView: ListView) :
+            AsyncTask<String, Void, String>() {
 
             private val TAG = "DownloadData"
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
 
             override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
 //                Log.d(TAG, "onPostExecute: parameter is $result")
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
+
+                val arrayAdapter = ArrayAdapter<FeedEntry>(
+                    propContext,
+                    R.layout.list_item,
+                    parseApplications.applications
+                )
+                propListView.adapter = arrayAdapter
             }
 
             override fun doInBackground(vararg url: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${url[0]}")
                 val rssFeed = downloadXML(url[0])
-                if (rssFeed.isEmpty()){
+                if (rssFeed.isEmpty()) {
                     Log.e(TAG, "doInBackground: Error downloading")
                 }
                 return rssFeed
             }
 
-            private fun downloadXML(urlPath: String?): String{
+            private fun downloadXML(urlPath: String?): String {
                 return URL(urlPath).readText()
             }
 
